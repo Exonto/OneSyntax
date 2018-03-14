c30ee1d3efeed99076732f2da0e080eb1d593096
LAPTOP-QU57SABP\tyler <tylersyme@gmail.com>
2018-03-14 11:07:54 -0700

    Created a custom angular material palette and theme

--- onesyntax/src/app/material.module.ts
+++ onesyntax/src/app/material.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
   ]
 })
 
